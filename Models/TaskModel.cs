414f5e446d45b122613b0b28eb5b5b4235076f25
Sameer Ahmed <ahmed139@sheridancollege.ca>
2023-11-03 22:14:38 -0400

    model created

---
+++ Models/TaskModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace GroupProject.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string TaskName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CompletionDate { get; set; }
+    }
+}
+
+
+
+
+
+
